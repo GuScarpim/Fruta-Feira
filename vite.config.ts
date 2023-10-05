300aba59011bb75af8aff98d8e841e89ec990181
Gustavo Scarpim <47616551+GuScarpim@users.noreply.github.com>
2023-10-05 23:10:24 +0000

    Configurando projeto

--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,6 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import tsConfigPaths from 'vite-tsconfig-paths'
-import svgrPlugin from 'vite-plugin-svgr'
 
-// https://vitejs.dev/config/
-export default defineConfig(configEnv => ({
-  plugins: [
-    react(),
-    tsConfigPaths(),
-    svgrPlugin(),
-  ],
-}))
+export default defineConfig({
+  plugins: [react()],
+})
